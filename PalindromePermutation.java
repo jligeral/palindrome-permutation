336547816ea0372d0172ea3368d7d7a1969b201d
James Ligeralde <jligeral@hawaii.edu>
2024-06-15 07:17:42 -1000

    Completed

--- PalindromePermutation.java
+++ PalindromePermutation.java
@@ -6,4 +6,48 @@
  */
 
 public class PalindromePermutation {
+    public static void main(String[] args) {
+        System.out.println(isPalindromePermutation("Tact Coa"));
+        System.out.println(isPalindromePermutation("Tact Coay"));
+        System.out.println(isPalindromePermutation("Tact Coao"));
+    }
+
+    public static boolean isPalindromePermutation(String str) {
+        int[] table = buildCharFrequencyTable(str);
+        return checkMaxOneOdd(table);
+    }
+
+    public static int getCharNumber(Character c) {
+        int a = Character.getNumericValue('a');
+        int z = Character.getNumericValue('z');
+        int val = Character.getNumericValue(c);
+        if (a <= val && val <= z) {
+            return val - a;
+        }
+        return -1;
+    }
+
+    public static int[] buildCharFrequencyTable(String str) {
+        int[] table = new int[Character.getNumericValue('z') - Character.getNumericValue('a') + 1];
+        for (char c : str.toCharArray()) {
+            int x = getCharNumber(c);
+            if (x != -1) {
+                table[x]++;
+            }
+        }
+        return table;
+    }
+
+    public static boolean checkMaxOneOdd(int[] table) {
+        boolean foundOdd = false;
+        for (int count : table) {
+            if (count % 2 == 1) {
+                if (foundOdd) {
+                    return false;
+                }
+                foundOdd = true;
+            }
+        }
+        return true;
+    }
 }
